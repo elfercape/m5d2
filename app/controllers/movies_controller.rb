82484d2c93318a9263fc53895e9e555338606801
fernando Lopes <elfercape@gmail.com>
2024-09-26 23:08:12 -0300

    v01

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,26 @@
+class MoviesController < ApplicationController
+
+  def index
+@movie_all = Movie.all
+end
+
+  
+    def create
+      @movie = Movie.new(movies_params)
+      if @movie.save
+      redirect_to movies_index_path
+      else
+      render :new
+      end
+    end
+  
+
+  def new
+    @movie = Movie.new
+  end
+
+  private
+   def movies_params
+   params.require(:movie).permit(:name, :synopsis, :director)
+  end
+end
